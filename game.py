422c7eb02de6e2e0454b87770620af3b37db2491
David Thomson <davidthomson123@hotmail.com>
2022-12-30 14:41:53 +0000

    add game class

---
+++ game.py
@@ -0,0 +1,40 @@
+from player_super import PlayerSuper
+from art import logo
+
+
+class Game:
+    def __init__(self):
+        self.player = []
+        self.dealer = []
+        self.play_again = True
+        self.logo = logo
+        return
+
+    def add_dealer(self, dealer: PlayerSuper):
+        return
+
+    def add_player(self, player: PlayerSuper):
+        return
+
+    def deal_card(self, player:PlayerSuper):
+        return
+
+    def choose_hit_or_stand(self, player: PlayerSuper):
+        return
+
+    def display_start(self):
+        return
+
+    def display_end(self):
+        return
+
+    def ask_play_again(self):
+        return
+
+    def display_winner(self):
+        return
+
+    def game_loop(self):
+        return
+
+
